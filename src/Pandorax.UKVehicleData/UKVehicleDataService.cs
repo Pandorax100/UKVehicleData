fca47493c9b32e1b1a72683a95196609f56f3560
Liam McDermott <44736094+Pandorax100@users.noreply.github.com>
2021-11-03 12:49:32 +0000

    Add logging

--- src/Pandorax.UKVehicleData/UKVehicleDataService.cs
+++ src/Pandorax.UKVehicleData/UKVehicleDataService.cs
@@ -1,6 +1,7 @@
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Pandorax.UKVehicleData.Models;
 
@@ -9,6 +10,7 @@ namespace Pandorax.UKVehicleData
     public class UKVehicleDataService : IUKVehicleDataService
     {
         private readonly HttpClient _client;
+        private readonly ILogger<UKVehicleDataService> _logger;
         private readonly UKVehicleDataOptions _options;
 
         /// <summary>
@@ -16,18 +18,27 @@ namespace Pandorax.UKVehicleData
         /// </summary>
         /// <param name="client">The <see cref="HttpClient"/> with which to make API calls.</param>
         /// <param name="options">The options accessor.</param>
-        public UKVehicleDataService(HttpClient client, IOptions<UKVehicleDataOptions> options)
+        /// <param name="logger">The logger instance for this class.</param>
+        public UKVehicleDataService(
+            HttpClient client,
+            IOptions<UKVehicleDataOptions> options,
+            ILogger<UKVehicleDataService> logger)
         {
             _client = client;
+            _logger = logger;
             _options = options.Value;
         }
 
         /// <inheritdoc/>
         public async Task<UKVehicleDataResponse> GetVehicleDataByRegistrationNumberAsync(string registration)
         {
+            _logger.LogDebug("Getting response for {registration}", registration);
+
             using HttpResponseMessage response = await _client.GetAsync($"/api/datapackage/VehicleData?v=2&auth_apikey={_options.ApiKey}&key_VRM={registration}");
 
             string json = await response.Content.ReadAsStringAsync();
+
+            _logger.LogDebug("Response for {registration}\n{json}", registration, json);
 
             var deserialized = JsonSerializer.Deserialize<UKVehicleDataResponse>(json);
 
